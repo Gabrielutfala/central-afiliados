dba2dfc1e54864db82ef85a9cdb1fe6138489dce
Gabrielutfala <lutfala53@gmail.com>
2026-08-24 19:22:52 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE="central-afiliados-v1";
+const ASSETS=["/","/index.html","/style.css","/app.js","/manifest.json","/icon-192.svg","/icon-512.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET") return;
+ e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
+});
